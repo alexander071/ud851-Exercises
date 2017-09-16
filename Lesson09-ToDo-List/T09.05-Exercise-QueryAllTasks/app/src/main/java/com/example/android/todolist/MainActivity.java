3194f62abf9b7571a5e093456cf6eef2fed2c440
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2017-09-16 15:50:56 +0300

    Solve exercise T09.05

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.android.todolist;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -27,7 +28,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
+
+import com.example.android.todolist.data.TaskContract;
 
 
 public class MainActivity extends AppCompatActivity implements
@@ -144,12 +148,17 @@ public class MainActivity extends AppCompatActivity implements
             // loadInBackground() performs asynchronous loading of data
             @Override
             public Cursor loadInBackground() {
-                // Will implement to load data
-
-                // TODO (5) Query and load all task data in the background; sort by priority
-                // [Hint] use a try/catch block to catch any errors in loading data
-
-                return null;
+                try {
+                    ContentResolver contentResolver = getContentResolver();
+                    return contentResolver.query(TaskContract.TaskEntry.CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            TaskContract.TaskEntry.COLUMN_PRIORITY);
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not get all tasks sorted by their priority.", e);
+                    return null;
+                }
             }
 
             // deliverResult sends the result of the load, a Cursor, to the registered listener
